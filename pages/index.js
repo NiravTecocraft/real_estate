a332f0970c6b761700223f8a52b7e93f44866d32
NiravTecocraft <nirav.lathiya@tecocraft.com>
2022-10-18 10:50:50 +0530

    added for demo

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import { Flex, Box, Text, Button } from '@chakra-ui/react';
 import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
+console.log('hell');
+
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
     <Image src={imageUrl} width={500} height={300} />
